36637102b29fd9d617fcee6f8da534aa4426e773
ettorebottega <ettore.bottega@campari.com>
2017-06-12 10:40:05 +0200

    My first commit
    I modified the .dpr (program source) and added a new unit (with a simple class)

--- testProgram.dpr
+++ testProgram.dpr
@@ -5,11 +5,15 @@ program testProgram;
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils,
+  System.IOUtils,
+  testUnit in 'Units\testUnit.pas';
 
 begin
   try
     WriteLn('Hello world!');
+
+    TFile.AppendAllText('testfile.txt', 'Message written.');
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
